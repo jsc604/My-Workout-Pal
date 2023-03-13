9f41543d882e7fd6dbc3561c238e123b7105ecf9
Jeffrey Cheung <jeffreycheung_@live.com>
2023-03-13 15:58:25 -0700

    fixed edit workout to match create workout

--- screens/EditWorkout.tsx
+++ screens/EditWorkout.tsx
@@ -1,8 +1,8 @@
 import { FunctionComponent, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import styled from "styled-components/native";
-import { TextInput, ScrollView, View } from "react-native";
-import DropDownPicker from 'react-native-dropdown-picker';
+import { TextInput, ScrollView, View, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 // firebase
 import firebase from 'firebase/compat/app';
@@ -14,8 +14,6 @@ const { firestore, auth } = firebase;
 import { Container } from "../components/shared";
 import RegularButton from "../components/buttons/RegularButton";
 import { colors } from "../components/colors";
-import { sampleExercises } from "../assets/workouts/exercises";
-import BigText from "../components/texts/BIgText";
 import RegularText from "../components/texts/RegularText";
 
 // navigation
@@ -32,17 +30,6 @@ import { updateDoc } from "../helpers/databaseHelpers";
 const EditWorkoutContainer = styled(Container)`
 `;
 
-const WorkoutInputs = styled(Container)`
-  margin: 20px auto;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  width: 60%;
-  height: fit;
-  flex: 0.1;
-  z-index: 2;
-`;
-
 const ExerciseInput = styled(TextInput)`
   height: 40px;
   border: 1px solid ${colors.black};
@@ -53,14 +40,8 @@ const ExerciseInput = styled(TextInput)`
 
 const EditWorkout: FunctionComponent<Props> = ({ navigation, route }) => {
   const { name, exercises } = route.params;
-  
-  const workoutExercises = exercises.map((item) => {
-    return item.exercise;
-  })
 
   const [workoutName, setWorkoutName] = useState(name);
-  const [open, setOpen] = useState(false);
-  const [exercise, setExercise] = useState<string[]>(workoutExercises);
   const [workoutData, setWorkoutData] = useState<ExerciseBlock[]>(exercises);
 
   const listsRef = firestore()
@@ -73,23 +54,15 @@ const EditWorkout: FunctionComponent<Props> = ({ navigation, route }) => {
     updateDoc(listsRef, name, { exercises });
   };
 
-  type ExerciseArray = Array<ExerciseBlock>;
-
-  const handleAddExercises = () => {
-    const newWorkoutData = exercise.reduce((acc: ExerciseArray, exercise) => {
-      const existingExercise = workoutData.find(item => item.exercise === exercise);
-      if (existingExercise) {
-        acc.push(existingExercise);
-      } else {
-        acc.push({ exercise, sets: 0, reps: 0 });
-      }
-      return acc;
-    }, []);
-
-    setWorkoutData(newWorkoutData);
+  const handleAddRow = () => {
+    setWorkoutData(prevData => [...prevData, { exercise: '', sets: 0, reps: 0 }]);
   };
 
-
+  const handleExerciseChange = (index: number, value: string) => {
+    const newWorkoutData = [...workoutData];
+    newWorkoutData[index].exercise = value;
+    setWorkoutData(newWorkoutData);
+  };
   const handleSetChange = (index: number, value: number) => {
     const newWorkoutData = [...workoutData];
     newWorkoutData[index].sets = value;
@@ -102,20 +75,32 @@ const EditWorkout: FunctionComponent<Props> = ({ navigation, route }) => {
     setWorkoutData(newWorkoutData);
   };
 
+  const handleDeleteRow = (index: number) => {
+    const newWorkoutData = [...workoutData];
+    newWorkoutData.splice(index, 1);
+    setWorkoutData(newWorkoutData);
+  };
+
   const workoutDataRow = workoutData.map((item, i) => {
     return (
       <View key={i + 600} style={{ flexDirection: 'row', width: '100%', marginTop: 10, alignItems: 'center' }}>
-        <RegularText textStyles={{ width: '50%' }}>{item.exercise}</RegularText>
         <ExerciseInput
-          style={{ width: '20%', margin: 'auto' }}
+          style={{ width: '50%' }}
+          onChangeText={(value) => handleExerciseChange(i, value)}
+          placeholder={item.exercise} />
+        <ExerciseInput
+          style={{ width: '18%', margin: 'auto' }}
           onChangeText={(value) => handleSetChange(i, parseInt(value))}
           placeholder={`${item.sets}`}
           keyboardType='numeric' />
         <ExerciseInput
-          style={{ width: '20%', margin: 'auto' }}
+          style={{ width: '18%', margin: 'auto' }}
           onChangeText={(value) => handleRepChange(i, parseInt(value))}
           placeholder={`${item.reps}`}
           keyboardType='numeric' />
+        <TouchableOpacity onPress={() => handleDeleteRow(i)}>
+          <Ionicons name="trash-outline" size={30} color="red" />
+        </TouchableOpacity>
       </View>
     )
   });
@@ -125,48 +110,28 @@ const EditWorkout: FunctionComponent<Props> = ({ navigation, route }) => {
       <StatusBar style="light" />
       <TextInput
         onChangeText={setWorkoutName}
-        placeholder="Update workout name"
-        style={{ marginTop: 20, fontSize: 20, borderColor: 'black', borderWidth: 1, borderRadius: 6 }}
+        placeholder={workoutName}
+        style={{ marginTop: 20, fontSize: 20, borderColor: 'black', borderWidth: 1, borderRadius: 6, textAlign: 'center' }}
       />
-      <WorkoutInputs>
-        <DropDownPicker
-          items={sampleExercises.map(exercise => ({
-            label: exercise.label,
-            value: exercise.label
-          }))}
-          multiple={true}
-          placeholder="Select exercises"
-          labelStyle={{
-            textAlign: 'left',
-            fontSize: 20
-          }}
-          setValue={setExercise}
-          searchable={true}
-          value={exercise}
-          open={open}
-          setOpen={setOpen}
-          mode={"BADGE"}
-        />
-        <RegularButton
-          onPress={handleAddExercises}
-          btnStyles={{ width: '30%', marginLeft: 20 }}
-        >
-          +
-        </RegularButton>
-      </WorkoutInputs>
-
-      {workoutName && <BigText>{workoutName}</BigText>}
 
       {workoutData.length > 0 &&
         (<ScrollView style={{ width: "90%", flex: 1, marginBottom: 20 }}>
           <View style={{ alignItems: "center" }}>
             <View style={{ flexDirection: 'row', width: '100%', borderBottomWidth: 1, marginTop: 10 }}>
               <RegularText textStyles={{ width: '50%' }}>Exercise</RegularText>
-              <RegularText textStyles={{ width: '25%', textAlign: 'center' }}>Sets</RegularText>
-              <RegularText textStyles={{ width: '25%', textAlign: 'center' }}>Reps</RegularText>
+              <RegularText textStyles={{ width: '20%', textAlign: 'center' }}>Sets</RegularText>
+              <RegularText textStyles={{ width: '20%', textAlign: 'center' }}>Reps</RegularText>
+              <RegularText textStyles={{ width: '10%', textAlign: 'center' }}>-</RegularText>
             </View>
             {workoutDataRow}
           </View>
+
+          <TouchableOpacity
+            onPress={handleAddRow}
+            style={{ marginVertical: 20, width: 'fit-content', borderRadius: 999, marginHorizontal: 'auto' }}
+          >
+            <Ionicons name="add-circle-outline" size={50} color={colors.black} />
+          </TouchableOpacity>
         </ScrollView>)}
 
       <RegularButton
